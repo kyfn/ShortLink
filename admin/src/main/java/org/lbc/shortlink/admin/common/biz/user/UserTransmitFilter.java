fd38a4da2e5089e4f551bbdb9588a6b1939336c6
xiaoqi <xiaoqi@qq.com>
2026-05-13 11:19:28 +0800

    feat: 用户信息封装上下文

---
+++ admin/src/main/java/org/lbc/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,38 @@
+package org.lbc.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.io.IOException;
+
+/**
+ * 用户信息传输过滤器
+ */
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String username = httpServletRequest.getHeader("username");
+        String token = httpServletRequest.getHeader("token");
+        if (username != null || token != null) {
+            Object userJsonStrObj = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userJsonStrObj != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userJsonStrObj.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+}
