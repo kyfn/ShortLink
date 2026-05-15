ad30fb24c21bf84985e0d95b7e301bb2aaf209d4
xiaoqi <xiaoqi@qq.com>
2026-05-15 09:44:56 +0800

    feat: mapper

---
+++ project/src/main/java/org/lbc/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,7 @@
+package org.lbc.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.lbc.shortlink.project.dao.entity.ShortLinkDO;
+
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
