e43818cbb69f9a5b2beff0f6cbb2217f096f35d9
xiaoqi <xiaoqi@qq.com>
2026-05-15 09:45:15 +0800

    feat: 启动类

---
+++ project/src/main/java/org/lbc/shortlink/project/ShortLinkProjectApplication.java
@@ -0,0 +1,14 @@
+package org.lbc.shortlink.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.lbc.shortlink.project.dao.mapper")
+public class ShortLinkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkProjectApplication.class, args);
+    }
+}
