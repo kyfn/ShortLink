35409a94c30c2081b675018167e7580537d65925
xiaoqi <xiaoqi@qq.com>
2026-05-18 15:22:01 +0800

    feat:  数据实体

---
+++ admin/src/main/java/org/lbc/shortlink/admin/remote/dto/ValidDate.java
@@ -0,0 +1,6 @@
+package org.lbc.shortlink.admin.remote.dto;
+
+public interface ValidDate {
+    Integer getValidDateType();
+    Long getValidDate();
+}
---
+++ admin/src/main/java/org/lbc/shortlink/admin/remote/dto/resp/ShortLinkRespDTO.java
@@ -0,0 +1,31 @@
+package org.lbc.shortlink.admin.remote.dto.resp;
+
+import lombok.Data;
+
+@Data
+public class ShortLinkRespDTO {
+    //分组 id
+    private String gid;
+    //域名
+    private String domain;
+    //短链接
+    private String shortUri;
+    //完整短链接
+    private String fullShortUrl;
+    //原始链接
+    private String originUrl;
+    //网站图标
+    private String favicon;
+    //点击量
+    private Integer clickNum;
+    //启用标识： 0 未启用，1 启用
+    private Integer statusEnable;
+    //创建类型：0 接口创建，1 控制台创建
+    private Integer createdType;
+    //有效期类型：0 永久有效 1 自定义
+    private Integer validDateType;
+    //有效期时间
+    private String validDate;
+    //描述
+    private String remark;
+}
